de7cf0d43149571410d628177a254eba91defb5e
Adrian Rigby <agjrigby@gmail.com>
2025-03-18 14:03:10 -0400

    flipped function call

--- src/agent/agent_main.py
+++ src/agent/agent_main.py
@@ -76,7 +76,7 @@ def get_agent(db, rules_db, cba_db, api_key, llm):
         The lower bound and upper bound of the range are the same if a single season is requested. Otherwise pass the bounds of the range.
         if a season type is not provided, we will assume the season type to be regular season. season_type can take the values 'regular', 'playoffs', or 'all'.
         if a situation is not provided, we will assume the situation to be all situations, situation can take the values, '5on5', '5on4', '4on5', or 'all'"""
-        goal_map_scatter_get(db, api_key, llm, conditions, season_lower_bound, season_upper_bound, season_type, situation)
+        goal_map_scatter_get(db, api_key, llm, conditions, season_lower_bound, season_upper_bound, situation, season_type)
         return "Goal map scatter plot generated successfully"
 
     @tool(args_schema=goal_map_scatter_schema)
@@ -94,7 +94,7 @@ def get_agent(db, rules_db, cba_db, api_key, llm):
         It is the same as goal_map_scatter but for shots. It uses the same schema and arguments.
         if a season type is not provided, we will assume the season type to be regular season. season_type can take the values 'regular', 'playoffs', or 'all'.
         if a situation is not provided, we will assume the situation to be all situations, situation can take the values, '5on5', '5on4', '4on5', or 'all'"""
-        heat_map_get(db, api_key, llm, conditions, all_shots, season_lower_bound, season_upper_bound, season_type, situation)
+        heat_map_get(db, api_key, llm, conditions, all_shots, season_lower_bound, season_upper_bound, situation, season_type)
         return "Heatmap generated successfully"
 
 
