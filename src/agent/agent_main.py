ba1f8960aee05860fda3370fceaabe2c6f1b5461
Adrian Rigby <agjrigby@gmail.com>
2025-03-19 12:05:33 -0400

    added description of possible playoff input format

--- src/agent/agent_main.py
+++ src/agent/agent_main.py
@@ -16,7 +16,7 @@ class goal_map_scatter_schema(BaseModel):
     season_lower_bound: int = Field(title="Season_lower_bound", description="""The first season in the range of seasons to generate the goal map scatter plot for. 
                                     Often a season can be refered to using two different years since it takes place on either side of new years,  like 'in the 2020-2021 season', 
                                     pass the first year as the argument. Another way this could be done is by only using the last two numbers of the second year. For example 2020-21 means pass '2020'
-                                    Pass this as ONLY the integer value. So if the user asks for the 2022 season. Pass the argument '2022'. 
+                                    Pass this as ONLY the integer value. So if the user asks for the 2022 season. Pass the argument '2022'. That said, if someone asks for a stat in the 2023 playoffs, they are refering to the 2022-23 season. So use 2022 as the season.
                                     DO NOT PASS 'Season 2022' Pass '2022'. When a range of seasons is provided like, 'from the 2015 to 2023 season generate a scatterplot' 
                                     This is the lower bound of the range. ie. the older year should be this value.'""")
     season_upper_bound: int = Field(title="Season_upper_bound", description="""The second season in the range of seasons to generate the goal map scatter plot for. 
@@ -24,7 +24,7 @@ class goal_map_scatter_schema(BaseModel):
                                     If there is a range of seasons in the request, then this should be the newest year. For example if someone asks 'generate a scatterplot for goals from the 2017-2023 seasons' 
                                     then this would take the value 2023. Someone may also say, 'generate a scatterplot from 2017-18 to 2022-23. Then the value of this would be 2022. Allways pass the fist year 
                                     if the season is given as multiple years. Interperate whether a range of seasons or single season is being requested. If it is a range, the upper bound should be this value.
-                                    If it is a single, this value should be the same as lower_bound_season. 
+                                    If it is a single, this value should be the same as lower_bound_season. Also note that if someone asks for a stat that is in the 2024 playoffs. This is actually from the season 2023-24.
                                     DO NOT PASS 'Season 2022' Pass '2022'""")
     season_type: str = Field(title="Season Type", description="""The type of season this should be past as: 'regular', 'playoffs', or 
                              'all'. Default to passing the word 'regular' if it is not specified. The playoffs can also be called the
